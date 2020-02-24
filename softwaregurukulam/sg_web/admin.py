e0fb7e07849c1c4082e77e46e7091bb0ac7cad8e
Bhadrinath Vetry <54344138+bhadrinath95@users.noreply.github.com>
2020-02-25 02:30:39 +0530

    Add files via upload

---
+++ softwaregurukulam/sg_web/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from sg_web.models import Category,Course,Document
+
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Document)
---
+++ softwaregurukulam/sg_web/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class sg_webConfig(AppConfig):
+    name = 'sg_web'
